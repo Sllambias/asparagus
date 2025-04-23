1d7c56cb10f0f1b38adaa57a8650ecbcd75d92c9
LlambiasMBP <llambias@live.com>
2025-04-23 11:23:54 +0200

    Add Task Conversion

---
+++ asparagus/functional/task_conversion_and_preprocessing/metadata_generation.py
@@ -0,0 +1,43 @@
+import os
+from batchgenerators.utilities.file_and_folder_operations import save_json
+from yucca.pipeline.task_conversion.utils import get_identifiers_from_splitted_files, files_in_dir
+from asparagus.modules.dataclasses.preprocessing import PreprocessingConfig
+
+
+def generate_dataset_json(
+    output_file: str,
+    imagesTr_dir: str,
+    imagesTs_dir: str,
+    modalities: dict,
+    labels: dict,
+    dataset_name: str,
+    preprocessing_module: PreprocessingConfig = None,
+    label_hierarchy: dict = {},
+    tasks: list = [],
+    dataset_description: str = "",
+):
+    first_file = files_in_dir(imagesTr_dir)[0]
+    im_ext = os.path.split(first_file)[-1].split(os.extsep, 1)[-1]
+    train_identifiers = get_identifiers_from_splitted_files(imagesTr_dir, im_ext, tasks)
+
+    if imagesTs_dir is not None:
+        test_identifiers = get_identifiers_from_splitted_files(imagesTs_dir, im_ext, tasks)
+    else:
+        test_identifiers = []
+
+    labels = {str(i): labels[i] for i in labels.keys()} if labels is not None else None
+
+    json_dict = {}
+    json_dict["name"] = dataset_name
+    json_dict["description"] = dataset_description
+    json_dict["image_extension"] = im_ext
+    json_dict["modality"] = {str(i): modalities[i] for i in range(len(modalities))}
+    json_dict["labels"] = labels
+    json_dict["label_hierarchy"] = label_hierarchy
+    json_dict["tasks"] = tasks
+    json_dict["numTraining"] = len(train_identifiers)
+    json_dict["numTest"] = len(test_identifiers)
+    json_dict["test"] = test_identifiers
+    json_dict["preprocessing_module"] = preprocessing_module
+
+    save_json(json_dict, os.path.join(output_file))
---
+++ asparagus/paths.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+
+from batchgenerators.utilities.file_and_folder_operations import (
+    maybe_mkdir_p as ensure_dir_exists,
+)
+
+
+def var_is_set(var):
+    return var in os.environ.keys()
+
+
+def get_environment_variable(var):
+    load_dotenv()
+    if not var_is_set(var):
+        raise ValueError(f"Missing required environment variable {var}.")
+
+    path = os.environ[var]
+    ensure_dir_exists(path)
+    return path
+
+
+def get_source_path():
+    return get_environment_variable("ASPARAGUS_SOURCE")
+
+
+def get_data_path():
+    return get_environment_variable("ASPARAGUS_DATA")
+
+
+def get_models_path():
+    return get_environment_variable("ASPARAGUS_MODELS")
+
+
+def get_results_path():
+    return get_environment_variable("ASPARAGUS_RESULTS")
