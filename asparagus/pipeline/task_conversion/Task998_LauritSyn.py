43c75ee61e70850d8673556374c58f5527998781
LlambiasMBP <llambias@live.com>
2025-05-15 14:41:05 +0200

    add LauritSyn

---
+++ asparagus/pipeline/task_conversion/Task998_LauritSyn.py
@@ -0,0 +1,59 @@
+import os
+from batchgenerators.utilities.file_and_folder_operations import (
+    join,
+    maybe_mkdir_p as ensure_dir_exists,
+)
+from asparagus.functional.task_conversion_and_preprocessing import (
+    generate_dataset_json,
+    generate_path_json,
+    process_mri_case,
+    process_dwi_case,
+    process_pet_case,
+    detect_cases,
+    detect_final_cases,
+    get_image_and_metadata_output_paths,
+    get_bvals_and_bvecs_v1,
+    multiprocess_mri_dwi_pet_cases,
+    postprocess_standard_dataset,
+)
+from asparagus.paths import get_data_path
+from asparagus.modules.dataclasses.presets.preprocessing_presets import GBrainPreprocessingConfig
+import torch
+
+
+def convert():
+    task_name = "Task998_LauritSyn"
+    file_suffix = ".pt"
+
+    target_dir = join(get_data_path(), task_name)
+    ensure_dir_exists(target_dir)
+
+    torch.manual_seed(421890)
+    for i in range(500):
+        data = torch.FloatTensor(
+            1,
+            torch.randint(low=20, high=768, size=(1,)),
+            torch.randint(low=20, high=768, size=(1,)),
+            torch.randint(low=20, high=768, size=(1,)),
+        ).uniform_(-1.1e37, 1.1e37)
+
+        torch.save(data, os.path.join(target_dir, f"LauritSyn_{i}.pt"))
+
+    postprocess_standard_dataset(
+        target_dir=target_dir,
+        file_suffix=file_suffix,
+        task_name=task_name,
+        DWI_patterns=[],
+        PET_patterns=[],
+        exclusion_patterns=[],
+        source_files_standard=[],
+        source_files_DWI=[],
+        source_files_PET=[],
+        source_files_excluded=[],
+        preprocessing_config=None,
+        processes=1,
+    )
+
+
+if __name__ == "__main__":
+    convert()
