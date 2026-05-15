02e6bfc75e740cf63dff75546d79434bef808a21
lukasugar <lukasugar@gmail.com>
2026-05-15 17:53:47 +0200

    Allow setting wandb project names

--- asparagus/pipeline/run/finetune_cls.py
+++ asparagus/pipeline/run/finetune_cls.py
@@ -51,7 +51,7 @@ def main(cfg: DictConfig) -> None:
         version=version_store.version,
         wandb_config=logging_safe_cfg,
         wandb_experiment=HydraConfig.get().job.config_name,
-        wandb_project="Finetune",
+        wandb_project=cfg.logger.wandb_project,
         wandb_logging=cfg.logger.wandb_logging,
         mlflow_logging=cfg.logger.mlflow_logging,
         log_to_stdout=cfg.logger.log_to_stdout,
